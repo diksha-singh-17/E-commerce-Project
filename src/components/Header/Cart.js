d5cef829a2fcc9b619ccebcb0ea82fde4a3ec808
Diksha Singh <dikshasinghrathore1107@gmail.com>
2024-05-04 00:46:42 +0530

    implemented cart items

--- src/components/Header/Cart.js
+++ src/components/Header/Cart.js
@@ -1,7 +1,13 @@
 import React from "react";
-
+import Button from "react-bootstrap/Button";
+import CartComponent from "./CartComponent";
 const Cart = () => {
-  return <div>Cart</div>;
+  return (
+    <div className="d-flex justify-content-end">
+      {/* <Button>Cart</Button> */}
+      <CartComponent />
+    </div>
+  );
 };
 
 export default Cart;
